bdcc7650d9871073a95074101d19dad0bc0978b8
Michal Lipczynski <michal.lipczynski@interia.pl>
2018-03-25 23:00:34 +0200

    Odczytt numeru tygodnia, odczyt dnia tygodnia (int), odczyt daty

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace START
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wk = WeekNumber.Get_Week_Number(DateTime.Now);
+            int day = (int)DateTime.Now.DayOfWeek;
+            string todayDate = DateTime.Now.ToString("d-MM-yyyy");
+            Console.WriteLine(wk);
+            Console.WriteLine(day);
+            Console.WriteLine(todayDate);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WeekNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace START
+{
+    class WeekNumber
+    {
+        public static int Get_Week_Number(DateTime date)
+        {
+           return CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(date, CultureInfo.CurrentCulture.DateTimeFormat.CalendarWeekRule, CultureInfo.CurrentCulture.DateTimeFormat.FirstDayOfWeek);
+        }
+    }
+}
