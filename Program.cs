18406a75fa751750ce2178c75f9dba4b6d0304e9
Michal Lipczynski <michal.lipczynski@interia.pl>
2018-03-27 23:02:40 +0200

    utworzenie klasy ExcelFile

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
 namespace START
 {
     class Program
@@ -14,6 +17,7 @@ namespace START
             int wk = WeekNumber.Get_Week_Number(DateTime.Now);
             int day = (int)DateTime.Now.DayOfWeek;
             string todayDate = DateTime.Now.ToString("d-MM-yyyy");
+
             Console.WriteLine(wk);
             Console.WriteLine(day);
             Console.WriteLine(todayDate);
